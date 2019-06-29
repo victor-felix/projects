be1232069522e7f4d27fe94bb342ab854543cb5f
Victor Felix <victorfelix.dev@gmail.com>
2019-06-28 21:57:03 -0300

    Adicionando atributo id nos objetos

--- index.js
+++ index.js
@@ -14,10 +14,12 @@ server.use((req, res, next) => {
 
 const projects = [
   {
+    id: "1",
     title: "Avengers: The Initiative",
     tasks: ["Save the World", "Save Asgard"]
   },
   {
+    id: "2",
     title: "Jedi Academy",
     tasks: ["Train the Force", "Conquering the Empire"]
   }
@@ -26,11 +28,13 @@ const projects = [
 function checkProjectExists(req, res, next) {
   const { id } = req.params;
 
-  if (!projects[id]) {
-    return res.status(400).json({ error: "Project not found" });
-  }
+  projects.map(project => {
+    if (project.id === id) {
+      return next();
+    }
+  });
 
-  return next();
+  return res.status(400).json({ error: "Project not found" });
 }
 
 server.get("/projects", (req, res) => {
@@ -38,10 +42,12 @@ server.get("/projects", (req, res) => {
 });
 
 server.post("/projects", (req, res) => {
+  const { id } = req.body;
   const { title } = req.body;
   const { tasks } = req.body;
 
   projects.push({
+    id: id,
     title: title,
     tasks: tasks
   });
@@ -53,7 +59,11 @@ server.put("/projects/:id", checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
 
-  projects[id]["title"] = title;
+  projects.map(project => {
+    if (project.id === id) {
+      project.title = title;
+    }
+  });
 
   return res.json(projects);
 });
@@ -61,7 +71,11 @@ server.put("/projects/:id", checkProjectExists, (req, res) => {
 server.delete("/projects/:id", checkProjectExists, (req, res) => {
   const { id } = req.params;
 
-  projects.splice(id, 1);
+  projects.map((project, index) => {
+    if (project.id === id) {
+      projects.splice(index, 1);
+    }
+  });
 
   return res.send();
 });
@@ -70,7 +84,11 @@ server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
   const { id } = req.params;
   const { task } = req.body;
 
-  projects[id]["tasks"].push(task);
+  projects.map(project => {
+    if (project.id === id) {
+      project.tasks.push(task);
+    }
+  });
 
   return res.json(projects);
 });
